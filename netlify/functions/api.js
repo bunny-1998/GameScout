7216063663193fa2d79b7121307e1d5e8bb86a27
bunny-1998 <zenithqa101@gmail.com>
2026-09-14 18:05:24 +0500

    Delete netlify/functions/api.js

--- netlify/functions/api.js
+++
@@ -1,9 +0,0 @@
-import serverless from "serverless-http";
-import express from "express";
-import path from "node:path";
-import app from "../../api/index.js";
-
-// Netlify copies `public/**` alongside the bundled function.
-app.use(express.static(path.join(process.cwd(), "public")));
-
-export const handler = serverless(app);
